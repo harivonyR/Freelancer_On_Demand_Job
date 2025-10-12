a3ad61ad42b6bfd40213fa2953feba137c0b5762
harivonyR* <harivonyratefiarison@gmail.com>
2025-10-12 19:51:58 +0300

    ADD query_builder, get pagination len ok

--- main.py
+++ main.py
@@ -5,7 +5,31 @@ Created on Thu Oct  9 19:24:38 2025
 @author: harivonyR
 """
 
+from script.freelancer import build_freelancer_query,get_total_result
 from script.piloterr import website_crawler
+from math import ceil
 
-freelancer_url = "https://www.freelancer.com/jobs/"
-res = website_crawler(freelancer_url)
+# Build Query (With Pagination PlaceHolder)
+query = build_freelancer_query(
+    keyword="script python",
+    fixed=True,
+    fixed_min=0,
+    fixed_max=10000,
+    contest=False,
+    languages=['en','fr','pt'])                              # list like ['en', 'fr']
+
+# Get The Page Limit
+result_len =  get_total_result(query.replace("{page}","2"))  # can only get a reliable result count on page2
+page_len = ceil(result_len / 50)                             # each page contain 50 results
+
+
+# Loop Over Pages
+page_result = []
+
+print("> freelancer scraping started !")
+for i in range(1, page_len + 1):
+    
+    res = website_crawler(query.replace("{page}", str(i)))
+    page_result.append(res)
+    
+    print(f"page {i} scraped !")
--- script/freelancer.py
+++ script/freelancer.py
@@ -5,16 +5,15 @@ Created on Sat Oct 11 19:44:36 2025
 @author: harivonyR
 """
 
-
 """
-Search Parameter : 
+Search Parameters : 
     key word : "python script"
     eg : https://www.freelancer.com/jobs/?keyword=python%2520script
     
     location (hard with few advantages) : "United States"
     eg : https://www.freelancer.com/jobs/?vic=United%20States&country=United%20States&lat=38.7945952&lon=-106.5348379&local=true
     
-    Fixed price : 
+    Fixed price :
         True
         eg : https://www.freelancer.com/jobs/?fixed=true
     
@@ -37,28 +36,130 @@ Search Parameter :
             eg : https://www.freelancer.com/jobs/?hourly_duration=5
     
     Contest : 
+        TRUE 
+        eg : https://www.freelancer.com/jobs/?contest=true
         
-
+        Min Max
+        eg : https://www.freelancer.com/jobs/?contest_min=10&contest_max=15
+        
+    Language (separtor "%C"):
+        en : English
+        es : Spanish
+        de : German
+        fr : French
+        pt : Portuguese
+    
+        eg : https://www.freelancer.com/jobs/?languages=en%2Ces%2Cde%2Cfr%2Cpt
+    
+    Job State :
+        All Job, open and closed
+        https://www.freelancer.com/jobs/?status=all
+    
 """
 
-def frelancer_query_builder():
+from bs4 import BeautifulSoup
+from urllib.parse import urlencode
+from script.piloterr import website_crawler
+from math import ceil
+
+def build_freelancer_query(
+    keyword=None,
+    fixed=False,
+    fixed_min=None,
+    fixed_max=None,
+    hourly=False,
+    hourly_min=None,
+    hourly_max=None,
+    hourly_duration=None,
+    contest=False,
+    contest_min=None,
+    contest_max=None,
+    languages=None,       # list like ['en', 'fr']
+    status=None          # set to "all" to display closed job
+):
+    base_url = "https://www.freelancer.com/jobs/{page}/?"
+    params = {}
+
+    # --- 1. Keyword ---
+    if keyword:
+        params["keyword"] = keyword.replace(" ", "%20")
+        
+    # --- 2. Handle exclusivity ---
+    mode_count = sum([fixed, hourly, contest])
+    
+    if mode_count > 1:
+        raise ValueError(" [incorrect] Parameters 'fixed', 'hourly', and 'contest' are mutually exclusive")
+
+    # --- 3. Fixed Price ---
+    if fixed:
+        params["fixed"] = "true"
+        if fixed_min and fixed_max:
+            params.update({
+                "fixed_min": fixed_min,
+                "fixed_max": fixed_max
+            })
+
+    # --- 4. Hourly Project ---
+    if hourly:
+        params["hourly"] = "true"
+        if hourly_min and hourly_max:
+            params.update({
+                "hourly_min": hourly_min,
+                "hourly_max": hourly_max
+            })
+        if hourly_duration:
+            params["hourly_duration"] = hourly_duration
+
+    # --- 5. Contest ---
+    if contest:
+        params["contest"] = "true"
+        if contest_min and contest_max:
+            params.update({
+                "contest_min": contest_min,
+                "contest_max": contest_max
+            })
+
+    # --- 6. Languages ---
+    if languages:
+        params["languages"] = "%2C".join(languages)
+
+    # --- 7. Job status ---
+    if status:
+        params["status"] = "all"
+
+    # --- 8. Return full query ---
+    return base_url + urlencode(params, doseq=True)
+
+def get_total_result(query: str) -> int:
     """
-        build a correct string to search
+    Extract total number of results from HTML.
+    bug : the total number of page is stuck on 5000
     """
+    html = website_crawler(query)
+    
+    soup = BeautifulSoup(html, "html.parser")
+    total_result = soup.select_one("span#total-results")  # correction du sélecteur
+    
+    if not total_result:
+        return 0  # sécurité si l'élément n'existe pas
 
-    Returns
-    -------
-    None.
-
-    """
-    pass
-
-
-"""
-flow :
-
-    build search param -> API request -> get page number () -> loop over page -> scrape job listing 
+    # extraction et conversion en entier
+    text = total_result.get_text(strip=True)
+    digits = ''.join(filter(str.isdigit, text))  # garde uniquement les chiffres
+    
+    return int(digits) if digits else 0
 
 
-"""
-
+if __name__ == "__main__" :
+    
+    query = build_freelancer_query(
+        keyword="script python",
+        fixed=True,
+        fixed_min=0,
+        fixed_max=10000,
+        contest=False,
+        languages=['en','fr','pt'])       # list like ['en', 'fr']
+    
+    
+    result_len =  get_total_result(query.replace("{page}","2"))       # can only get result on page number 2
+    page_len = ceil(result_len / 50)                                  # there is 50 results each page
